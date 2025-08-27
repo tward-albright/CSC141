8b8507b918a1720f09545749541b3c59b0582ac9
tward-albright <taylan.ward001@albright.edu>
2025-08-27 09:15:33 -0400

    added if statements to variables.py

--- assignment_one/variables.py
+++ assignment_one/variables.py
@@ -2,3 +2,15 @@ y = 3
 x = 7 + y
 
 print(x)
+
+greeting = "20.5"
+print(greeting.isdecimal())
+
+temperature = 85
+
+if temperature > 75:
+    print("It's hot out!")
+elif temperature > 50:
+    print("It's nice out.")
+else:
+    print("It's cold out!")
