0958ea31893c5db810ea4dd39b3b582d51816663
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-09-01 09:02:49 -0400

    wrote week 2 scratch program

---
+++ scratch_files/week2.py
@@ -0,0 +1,11 @@
+# Taylan Ward
+# Checks the user's name against mine.
+
+# Get the name of the user.
+print("Please enter your name: ", end="")
+name = input()
+
+if name == "Taylan":
+    print(f"Hello, {name}!")
+else:
+    print(f"Hey, you're not me!")
