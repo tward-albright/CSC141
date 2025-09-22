6368e341990063148f246f2e1affde9879c411d7
tward-albright <taylan.ward001@albright.edu>
2025-09-22 08:00:35 -0400

    got started on assignment 4

---
+++ assignment_five/conditional_tests.py
@@ -0,0 +1,16 @@
+dog_breed = "labrador"
+print("Is dog_breed == 'labrador'? True")
+print(dog_breed == "labrador")
+
+print("\nIs dog_breed == 'shitzu'? False")
+print(dog_breed == "shitzu")
+
+print("Is dog_breed != 'labrador'? False")
+print(dog_breed != "labrador")
+
+secret_number = 450
+print("Is secret_number divisible by 5? True")
+print(secret_number % 5 == 0)
+
+print("Is secret_number divisible by 7? False")
+print(secret_number % 7 == 0)
