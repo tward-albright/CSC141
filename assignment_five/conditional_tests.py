fc3e8ba63cd15f76b7aea96e93641965a2387949
tward-albright <taylan.ward001@albright.edu>
2025-09-24 08:16:14 -0400

    Update conditional_tests.py

--- assignment_five/conditional_tests.py
+++ assignment_five/conditional_tests.py
@@ -1,3 +1,4 @@
+# 5-1 and 5-2
 dog_breed = "labrador"
 print("Is dog_breed == 'labrador'? True")
 print(dog_breed == "labrador")
@@ -8,9 +9,25 @@ print(dog_breed == "shitzu")
 print("Is dog_breed != 'labrador'? False")
 print(dog_breed != "labrador")
 
+print("Is len(dog_breed) > len('shitzu')? True")
+print(len(dog_breed) > len("shitzu"))
+
+print("Is dog_breed.istitle()? False")
+print(dog_breed.istitle())
+
 secret_number = 450
+
 print("Is secret_number divisible by 5? True")
 print(secret_number % 5 == 0)
 
 print("Is secret_number divisible by 7? False")
 print(secret_number % 7 == 0)
+
+print("Is double the secret number divisble by 4? True")
+print((secret_number * 2) % 4 == 0)
+
+print("Is secret_number > 50? True")
+print(secret_number > 50)
+
+print("Is secret_number / 10 == 40? False")
+print(secret_number / 10 == 40)
